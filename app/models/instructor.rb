f61a9b42643297116a99175df0932a8121547860
talhait07 <monsur.it07@gmail.com>
2013-09-25 22:28:18 +0600

    instructor page is in progress

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,4 +1,5 @@
 class Instructor < ActiveRecord::Base
-  attr_accessible :title, :sub_title, :summary, :created_by
+  attr_accessible :title, :sub_title, :summary, :created_by, :photo
   has_and_belongs_to_many :courses
+  has_attached_file :photo, :styles => {:small => '150*85 >'}
 end
