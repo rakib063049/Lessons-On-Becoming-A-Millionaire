bde3897658b8307a82e910583e34626f1ab27cd9
talhait07 <monsur.it07@gmail.com>
2013-09-28 01:05:41 +0600

    user dashboard added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,11 @@ class HomeController < ApplicationController
   def index
     render :layout => 'application'
   end
+  def dashboard
+    @lessons = Lesson.all(:limit => 6)
+    @instructors = Instructor.all
+    @categories = Category.all
+    @instructor = Instructor.all(:limit => 1, :order => "id DESC")
+    render :layout => 'application'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 LessonsOnBecomingAMillionaire::Application.routes.draw do
   root to: 'home#index'
+  match "home/dashboard" => 'home#dashboard', :as => "home/dashboard"
   match "pages/:id" => 'pages#show', :as => :page, :format => false
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
